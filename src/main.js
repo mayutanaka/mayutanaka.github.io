41567a1cb05dbef4fe816894d3d3d8ca2bd442b5
Mayu Tanaka <mayu5tanaka@gmail.com>
2019-05-16 00:05:56 -0400

    deleted files

--- src/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $('.page-fade').fadeIn();
-    var validModal = document.body.contains(document.getElementById("construction"));
-    if (validModal) {
-      $('#construction').modal('show');
-    }
-});
