20d5a8475bddac7c4b0de94ae0cec277cc8e05e0
Mayu Tanaka <mayu5tanaka@gmail.com>
2019-05-17 18:56:30 -0400

    carousel broken, resume subnav fixed

--- home/src/main.js
+++ home/src/main.js
@@ -1,7 +1,23 @@
+var subnavs = ['skills', 'education', 'experience', 'extracurriculars'];
+var previous;
+
 $(document).ready(function(){
     $('.page-fade').fadeIn();
     var validModal = document.body.contains(document.getElementById("construction"));
     if (validModal) {
       $('#construction').modal('show');
+    }
+
+    subnavs.forEach(function(current) {
+      $("#nav-"+current).click(function() {
+        if(previous) { document.getElementById("nav-"+previous).classList.remove("sub-select") }
+        document.getElementById("nav-"+current).classList.add("sub-select");
+        previous = current;
+      });
+    });
+
+    var thisPage = document.URL.split('/');
+    if (thisPage[thisPage.length-1]=='about-me.html') {
+      $('#bio-carousel').carousel('pause');
     }
 });
