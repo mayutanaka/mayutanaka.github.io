e0f41419fc5f0a612efab7dc1440622c08a45e1e
Mayu Tanaka <mayu5tanaka@gmail.com>
2019-05-23 17:22:52 -0700

    resume complete, about-me fixed

--- home/src/main.js
+++ home/src/main.js
@@ -2,7 +2,10 @@ var subnavs = ['skills', 'education', 'experience', 'extracurriculars'];
 var previous;
 
 $(document).ready(function(){
-    $('.page-fade').fadeIn();
+    $(function() {
+      $('body').removeClass('fade-out');
+    });
+    // $('.page-fade').fadeIn();
     var validModal = document.body.contains(document.getElementById("construction"));
     if (validModal) {
       $('#construction').modal('show');
@@ -20,4 +23,42 @@ $(document).ready(function(){
     if (thisPage[thisPage.length-1]=='about-me.html') {
       $('#bio-carousel').carousel('pause');
     }
+
+//https://css-tricks.com/snippets/jquery/smooth-scrolling/
+    // Select all links with hashes
+    $('a[href*="#"]')
+      // Remove links that don't actually link to anything
+      .not('[href="#"]')
+      .not('[href="#0"]')
+      .click(function(event) {
+        // On-page links
+        if (
+          location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+          &&
+          location.hostname == this.hostname
+        ) {
+          // Figure out element to scroll to
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          // Does a scroll target exist?
+          if (target.length) {
+            // Only prevent default if animation is actually gonna happen
+            event.preventDefault();
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000, function() {
+              // Callback after animation
+              // Must change focus!
+              var $target = $(target);
+              $target.focus();
+              if ($target.is(":focus")) { // Checking if the target was focused
+                return false;
+              } else {
+                $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+              };
+            });
+          }
+        }
+      });
 });
